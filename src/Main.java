88f3a0308ea575209a93d5fd3fecaeb233856cd6
30196000 <30196000@nescol.ac.uk>
2023-08-30 14:59:31 +0100

    Two types of reading

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        keyInput();
+        fileInput();
+    }
+
+    private static void keyInput() {
+        Scanner cs = new Scanner(System.in);
+        int a = cs.nextInt();
+        int b = cs.nextInt();
+        System.out.println(a + b);
+    }
+
+    private static void fileInput() throws IOException {
         BufferedReader bf = new BufferedReader(new FileReader("input.txt"));
         FileWriter fw = new FileWriter("output.txt");
         String[] numbers = bf.readLine().split(" ");
         int a = Integer.parseInt(numbers[0]);
         int b = Integer.parseInt(numbers[1]);
-        System.out.println(a + b);
         fw.write(String.valueOf(a+b));
         fw.close();
     }
